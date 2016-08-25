d9fa110a1c7056e78ba7bc7cc2b7ef3b7fd54835
ruiwang <ruiwang@ruiwangdeimac.local>
2016-08-25 15:01:26 +0800

    Initial Commit

---
+++ alertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  alertView
+//
+//  Created by ruiwang on 16/8/25.
+//  Copyright © 2016年 ruiwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ alertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  alertView
+//
+//  Created by ruiwang on 16/8/25.
+//  Copyright © 2016年 ruiwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
